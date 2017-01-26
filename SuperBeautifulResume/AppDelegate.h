5327922fe679117c6396a3de9b637ec0177e16b6
岳克奎 <738816656@qq.com>
2017-01-26 21:12:18 +0800

    Initial Commit

---
+++ SuperBeautifulResume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperBeautifulResume
+//
+//  Created by 岳克奎 on 17/1/26.
+//  Copyright © 2017年 岳克奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperBeautifulResume/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SuperBeautifulResume
+//
+//  Created by 岳克奎 on 17/1/26.
+//  Copyright © 2017年 岳克奎. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
